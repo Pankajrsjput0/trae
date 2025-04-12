cd8d467eb62f940adb78d2e3d044f866bc5bc059
Pankajrsjput0 <144032583+Pankajrsjput0@users.noreply.github.com>
2025-04-12 22:12:38 +0530

    Update SupabaseClient.kt

--- app/src/main/java/com/maka/launcher/data/SupabaseClient.kt
+++ app/src/main/java/com/maka/launcher/data/SupabaseClient.kt
@@ -5,8 +5,8 @@ import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.realtime.Realtime
 
 object SupabaseClient {
-    private const val SUPABASE_URL = "https://lrezxzngdtxzlmrrwjyl.supabase.co"
-    private const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyZXp4em5nZHR4emxtcnJ3anlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzOTc0MTAsImV4cCI6MjA1OTk3MzQxMH0.IQOHNDfmNOPW8n2ZxvuZGTqtCBbuhv7Stf_t9IuN7G4"
+    private const val SUPABASE_URL = "https:supabase.co"
+    private const val SUPABASE_ANON_KEY = "eyJhbStf_t9IuN7G4"
 
     val client = createSupabaseClient(
         supabaseUrl = SUPABASE_URL,
